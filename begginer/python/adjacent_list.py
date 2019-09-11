f8e5d993f61c6125d9dc63b111d4acfe63d85380
アルゴリズム <tomoki0819@akane.waseda.jp>
2019-09-11 12:39:51 +0900

    complete

---
+++ begginer/python/adjacent_list.py
@@ -0,0 +1,10 @@
+def main():
+    number_of_v,edge = [int(i) for i in input().split()]
+    adj_list = [[] for i in range(number_of_v)]
+    for i in range(edge):
+        s,e = [int(i) for i in input().split()]
+        adj_list[s].append(e)
+    print(adj_list)
+
+if __name__ == "__main__":
+    main()
