165007e404d3603e6a986a6904e78a1a9dc319c9
アルゴリズム <tomoki0819@akane.waseda.jp>
2019-10-04 11:32:49 +0900

    complete

--- begginer/python/dijkstra.py
+++ begginer/python/dijkstra.py
@@ -1,5 +1,5 @@
 import heapq
-INF = 1<<21
+INF = 100000000000000
 
 def make_adj_list(v):
     adj_list = []
@@ -7,44 +7,40 @@ def make_adj_list(v):
         adj_list.append([])
     return adj_list
 
-class Edge():
-    def __init__(self, to, cost):
-        self.to = to
-        self.cost = cost    
-
 def dijkstra(v, e, adj_list, s):
     heap = []
     d = []
     pre = []
     for i in range(v):
         d.append(INF)
-        pre.append(0)
-    d[0] = 0
+        pre.append(s)
+    d[s] = 0
     heapq.heappush(heap, (0, s))
     while len(heap) != 0:
         candidate_d, now_v = heapq.heappop(heap)
         if d[now_v] < candidate_d:
             continue
-
-        for v in adj_list[now_v]:
-            if d[v.to] > d[now_v] + v.cost:
-                d[v.to] = d[now_v] + v.cost
-                heapq.heappush(heap,(d[v.to], v.to))
-                pre[v.to] = now_v
+        for v, cost in adj_list[now_v]:
+            if d[v] > d[now_v] + cost:
+                d[v] = d[now_v] + cost
+                heapq.heappush(heap,(d[v], v))
+                pre[v] = now_v
     return d, pre
 
 def main():
-    v, e = map(int, input().split())
+    v, e, s = map(int, input().split())
     adj_list = make_adj_list(v)
     for i in range(e):
         a = [int(i) for i in input().split()]
-        adj_list[a[0]].append(Edge(a[1], a[2]))
-        adj_list[a[1]].append(Edge(a[0], a[2]))
+        adj_list[a[0]].append((a[1], a[2]))
         
-    d, pre = dijkstra(v, e, adj_list, 0)
+    d, pre = dijkstra(v, e, adj_list, s)
 
-    print(d)
-
+    for i in range(v):
+        if d[i] == INF:
+            print("INF")
+        else:
+            print(d[i])
 
 if __name__ == "__main__":
     main()
