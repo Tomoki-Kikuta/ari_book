28096ee220701fc5bf5d2e7a423572366d2a9955
アルゴリズム <tomoki0819@akane.waseda.jp>
2019-10-15 10:39:07 +0900

    not understand

--- begginer/python/carmichael_numbers.py
+++ begginer/python/carmichael_numbers.py
@@ -1,26 +1,23 @@
-import sys
-
 def mod_pow(x, n, mod):
-    if n == 1:
-        return x
-    res = mod_pow(x * x, int(n / 2), mod)
+    res = 1
+    while n > 0:
+        if n & 1:
+            print(n)
+            res = res * x % mod
+        x = x * x % mod
+        n >>= 1
     return res
+
 
 def main():
     n = int(input())
     for x in range(2, n):
-        xn = mod_pow(x, n, n)
-        xn_mod_n = 0
-        if n % 2 == 1:
-            xn_mod_n = (xn * x) % n
-        else:
-            xn_mod_n %= n
-        print("{} {}".format(xn_mod_n,x))
-        if x != xn_mod_n:
+        res = mod_pow(x, n, n)
+        if x != res:
             print("No")
             exit()
-
     print("Yes")
+
 
 if __name__ == "__main__":
     main()
