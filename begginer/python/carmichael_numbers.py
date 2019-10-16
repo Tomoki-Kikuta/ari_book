8cc8be226d1ede15e02cdb9172cd894f2a5c3c0b
アルゴリズム <tomoki0819@akane.waseda.jp>
2019-10-16 11:14:37 +0900

    complete

--- begginer/python/carmichael_numbers.py
+++ begginer/python/carmichael_numbers.py
@@ -2,18 +2,34 @@ def mod_pow(x, n, mod):
     res = 1
     while n > 0:
         if n & 1:
-            print(n)
             res = res * x % mod
         x = x * x % mod
         n >>= 1
     return res
 
 
+def is_prime(n):
+    if n == 1:
+        return False
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 1
+    return True
+
+
 def main():
     n = int(input())
+
+    if is_prime(n):
+        # nが素数であるとき
+        print("No")
+        exit()
+
     for x in range(2, n):
         res = mod_pow(x, n, n)
-        if x != res:
+        if x != res % n:
             print("No")
             exit()
     print("Yes")
