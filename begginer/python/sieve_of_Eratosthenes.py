a590c74002f66ad7cdd548f158a0bc673de4c5be
アルゴリズム <tomoki0819@akane.waseda.jp>
2019-10-18 18:29:25 +0900

    complete

--- begginer/python/sieve_of_Eratosthenes.py
+++ begginer/python/sieve_of_Eratosthenes.py
@@ -3,12 +3,16 @@ def main():
     prime = []
     is_prime = [True for i in range(N+1)]
     is_prime[0] = is_prime[1] = False
-    for i in range(2,N+1):
-        if is_prime[i] == True:
+    for i in range(2, N+1):
+        if is_prime[i]:
+            # iは素数である
             prime.append(i)
             for j in range(2 * i, N+1, i):
+                # iの倍数は素数でない
                 is_prime[j] = False
-    print(prime)
     # N以下の素数を表示
+    print(prime)
+
+
 if __name__ == "__main__":
     main()
