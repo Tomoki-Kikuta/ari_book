34a61ac0b82e2ebd4b62601f6729d64534722915
アルゴリズム <tomoki0819@akane.waseda.jp>
2019-10-22 19:48:23 +0900

    complete

--- begginer/python/Bellman_Ford.py
+++ begginer/python/Bellman_Ford.py
@@ -1,12 +1,14 @@
 INF = 100000000
 
+
 class Edge():
     def __init__(self, fro, to, cost):
         self.fro = fro
         self.to = to
         self.cost = cost
 
-def cal_path(v, e, edge):
+
+def cal_min_distance(v, e, edge):
     d = []
     for i in range(v):
         d.append(INF)
@@ -16,22 +18,25 @@ def cal_path(v, e, edge):
     while update:
         update = False
         for now_edge in edge:
-            if d[now_edge.fro] != INF and d[now_edge.to] > d[now_edge.fro] + now_edge.cost:
+            if (d[now_edge.fro] != INF and
+               d[now_edge.to] > d[now_edge.fro] + now_edge.cost):
                 d[now_edge.to] = d[now_edge.fro] + now_edge.cost
                 update = True
     return d
 
+
 def main():
     v, e = map(int, input().split())
-    edge= []
+    edge = []
     for i in range(e):
         a = [int(i) for i in input().split()]
         edge.append(Edge(a[0], a[1], a[2]))
 
-    d = cal_path(v, e, edge)
+    d = cal_min_distance(v, e, edge)
 
     for i in range(v):
         print(d[i])
+
 
 if __name__ == "__main__":
     main()
