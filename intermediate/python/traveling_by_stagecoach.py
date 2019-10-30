651b582800d0643c7435c8b0e13a6825c50880fd
アルゴリズム <tomoki0819@akane.waseda.jp>
2019-10-30 14:14:07 +0900

    complete

--- intermediate/python/traveling_by_stagecoach.py
+++ intermediate/python/traveling_by_stagecoach.py
@@ -1,13 +1,41 @@
-INF = 10 ** 12
+INF = 10 ** 6
 
 
-def make_adj_list(town, road):
-    adj_list = [[INF for i in range(town)] for j in range(town)]
+def make_d(town, road):
+    d = [[INF for i in range(town)] for j in range(town)]
     for i in range(road):
         s, t, distance = map(int, input().split())
-        adj_list[s][t] = distance
-        adj_list[t][s] = distance
-    return adj_list
+        s -= 1
+        t -= 1
+        d[s][t] = distance
+        d[t][s] = distance
+    return d
+
+
+def cal_min_time(coach_ticket, town, road, start, goal, d, head):
+    dp = []
+    # dpの初期化(かかる時間をINFとする)
+    for i in range(1 << coach_ticket):
+        dp.append([])
+        for j in range(town):
+            dp[i].append(INF)
+    # 全てのチケットが残っていて、スタートの街にいるとき、時間を0とする
+    dp[(1 << coach_ticket) - 1][start] = 0
+
+    res = INF
+    for S in range((1 << coach_ticket) - 1, -1, -1): 
+        res = min(res, dp[S][goal])
+        for i in range(town):
+            #　iの都市にいて、残っている乗車券の集合がSである
+            for j in range(coach_ticket):
+                if S >> j & 1:
+                    # j番目の乗車券が残っている時
+                    for u in range(town):
+                        # 乗車券jを使ってuに行く
+                        if d[i][u] >= 0:
+                            dp[S & ~(1 << j)][u] = min(dp[S & ~(1 << j)][u], dp[S][i] + d[i][u] / head[j])
+
+    return res
 
 
 def main():
@@ -15,8 +43,18 @@ def main():
         coach_ticket, town, road, start, goal = map(int, input().split())
         if coach_ticket == town == road == start == goal == 0:
             break
+        start -= 1
+        goal -= 1
         head = [int(i) for i in input().split()]
-        adj_list = make_adj_list(town, road)
+
+        d = make_d(town, road)
+
+        ans = cal_min_time(
+              coach_ticket, town, road, start, goal, d, head)
+        if ans > 100 * 30:
+            print("impossible")
+        else:
+            print("{:.3f}".format((round(ans, 3))))
 
 
 if __name__ == "__main__":
