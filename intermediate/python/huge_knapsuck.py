9d6389691b4ad52afd594d5095578d280348500e
アルゴリズム <tomoki0819@akane.waseda.jp>
2019-11-05 20:46:17 +0900

    complete

--- intermediate/python/huge_knapsuck.py
+++ intermediate/python/huge_knapsuck.py
@@ -26,36 +26,36 @@ def make_combination(fro, to, value, weight, W):
     return combination
 
 
-def cal_ans(from_0_to_harf_combination, from_harf_to_N_combination, W):
+def cal_ans(from_0_to_half_combination, from_half_to_N_combination, W):
     ans = 0
 
     """ 尺取り法 """
     left = 0
-    right = len(from_harf_to_N_combination) - 1
-    for left in range(len(from_0_to_harf_combination)):
-        now_weight = from_0_to_harf_combination[left][0]
-        now_value = from_0_to_harf_combination[left][1]
-        while now_weight + from_harf_to_N_combination[right][0] > W:
+    right = len(from_half_to_N_combination) - 1
+    for left in range(len(from_0_to_half_combination)):
+        now_weight = from_0_to_half_combination[left][0]
+        now_value = from_0_to_half_combination[left][1]
+        while now_weight + from_half_to_N_combination[right][0] > W:
             right -= 1
             if right < 0:
                 right = 0
                 break
-        if now_weight + from_harf_to_N_combination[right][0] <= W:
-            ans = max(ans, now_value + from_harf_to_N_combination[right][1])
+        if now_weight + from_half_to_N_combination[right][0] <= W:
+            ans = max(ans, now_value + from_half_to_N_combination[right][1])
 
     """ 二分探索 """
-    # for now in range(len(from_0_to_harf_combination)):
-    #     now_weight = from_0_to_harf_combination[now][0]
-    #     now_value = from_0_to_harf_combination[now][1]
+    # for now in range(len(from_0_to_half_combination)):
+    #     now_weight = from_0_to_half_combination[now][0]
+    #     now_value = from_0_to_half_combination[now][1]
     #     count = 0
     #     left = 0
-    #     right = len(from_0_to_harf_combination) - 1
+    #     right = len(from_0_to_half_combination) - 1
     #     while count < 45:
     #         mid = (left + right) // 2
-    #         if now_weight + from_harf_to_N_combination[mid][0] > W:
+    #         if now_weight + from_half_to_N_combination[mid][0] > W:
     #             right = mid - 1
     #         else:
-    #             ans = max(ans, now_value + from_harf_to_N_combination[mid][1])
+    #             ans = max(ans, now_value + from_half_to_N_combination[mid][1])
     #             left = mid + 1
     #         count += 1
 
@@ -64,7 +64,7 @@ def cal_ans(from_0_to_harf_combination, from_harf_to_N_combination, W):
 
 def main():
     N, W = map(int, input().split())
-    harf = N // 2
+    half = N // 2
     value = []
     weight = []
     for i in range(N):
@@ -73,11 +73,11 @@ def main():
         weight.append(w)
 
     # 半分全列挙の組み合わせを前半と後半で作る
-    from_0_to_harf_combination = make_combination(0, harf, value, weight, W)
-    from_harf_to_N_combination = make_combination(harf, N, value, weight, W)
+    from_0_to_half_combination = make_combination(0, half, value, weight, W)
+    from_half_to_N_combination = make_combination(half, N, value, weight, W)
 
     # 尺取り法か二分探索で計算
-    ans = cal_ans(from_0_to_harf_combination, from_harf_to_N_combination, W)
+    ans = cal_ans(from_0_to_half_combination, from_half_to_N_combination, W)
     print(ans)
 
 
