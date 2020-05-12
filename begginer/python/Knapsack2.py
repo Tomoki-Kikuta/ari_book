ad43f80f1e3ac94014a28531b9e6a116dae69294
アルゴリズム <tomoki0819@akane.waseda.jp>
2020-05-12 09:48:16 +0900

    complete

---
+++ begginer/python/Knapsack2.py
@@ -0,0 +1,21 @@
+def main():
+    n = int(input())
+    weight = []
+    value = []
+    for i in range(n):
+        w, v = map(int, input().split())
+        weight.append(w)
+        value.append(v)
+    W = int(input())
+    dp = [[0 for i in range(W+1)] for j in range(n+1)]
+    for i in range(1, n+1):
+        for j in range(1, W+1):
+            if weight[i - 1] > j:
+                dp[i][j] = dp[i - 1][j]
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight[i - 1]] + value[i - 1])
+    print(dp[n][W]) 
+
+
+if __name__ == "__main__":
+    main()
