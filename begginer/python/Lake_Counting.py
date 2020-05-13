35bef9b63b440b1e900a2d426476943d82070e81
アルゴリズム <tomoki0819@akane.waseda.jp>
2020-05-13 09:13:15 +0900

    complete

--- begginer/python/Lake_Counting.py
+++ begginer/python/Lake_Counting.py
@@ -1,12 +1,12 @@
-import queue
+from collections import deque
 
 
 def lake_counting(now_x, now_y, n, m, garden):
     garden[now_x][now_y] = '.'
-    next_go = queue.Queue()
-    next_go.put((now_x, now_y))
-    while not next_go.empty():
-        now_x, now_y = next_go.get() 
+    next_go = deque()
+    next_go.append((now_x, now_y))
+    while len(next_go) != 0:
+        now_x, now_y = next_go.pop() 
         for x in range(-1, 2):
             for y in range(-1, 2):
                 if  x == 0 and y == 0:
@@ -15,7 +15,7 @@ def lake_counting(now_x, now_y, n, m, garden):
                 next_y = now_y + y
                 if next_x >= 0 and next_x < n and next_y >= 0 and next_y < m and garden[next_x][next_y] == 'W':
                     garden[next_x][next_y] = '.'
-                    next_go.put((next_x, next_y))
+                    next_go.append((next_x, next_y))
 
 
 def main():
